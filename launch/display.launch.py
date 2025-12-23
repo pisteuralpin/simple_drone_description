d07ad3dde64de4555339f138a539006278547a01
Mathurin Roulier <roulier.mathurin@gmail.com>
2025-12-23 18:11:12 +0100

    ➕ Add display in RViz

---
+++ launch/display.launch.py
@@ -0,0 +1,35 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    pkg_share = FindPackageShare('pisteur_drone_description').find('pisteur_drone_description')
+    urdf_file = os.path.join(pkg_share, 'urdf', 'x3.urdf')
+    rviz_config = os.path.join(pkg_share, 'rviz', 'x3.rviz')
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': open(urdf_file).read()}],
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz_node,
+    ])
